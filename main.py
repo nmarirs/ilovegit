7ffd2028a1ca22ac1d8b459e66e374132159eab7
nmarirs <nmarirs@proton.me>
2024-04-10 23:40:53 +0530

    move token to env file
    haha that was a close one :p

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import discord
+import os
+from dotenv import load_dotenv
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -6,7 +8,7 @@ class MyClient(discord.Client):
 
     async def on_message(self, message):
         print(f"Message from {message.author}: {message.content}")
-        if "git" in message.content:
+        if "git" in message.content and not message.author.bot:
             await message.channel.send("Excuse me, would you like to hear about our lord and savior Git, the free and open source version control system? https://git-scm.com/")
 
 
@@ -14,4 +16,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = MyClient(intents=intents)
-client.run("cruXcrypt{git_st1ll_r3m3mb3r5!}")
+
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+client.run(TOKEN)
