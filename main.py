aa49b4567e51939913ab3c2f7192327a0c5933c0
nmarirs <nmarirs@proton.me>
2024-04-12 00:30:44 +0530

    add jokes

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import discord
 import os
 from dotenv import load_dotenv
+import random
+
+def random_joke():
+    with open("./jokes.txt", "r") as f:
+        jokes = f.readlines()
+        joke = random.choice(jokes)
+        return joke
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -8,7 +15,9 @@ class MyClient(discord.Client):
 
     async def on_message(self, message):
         print(f"Message from {message.author}: {message.content}")
-        if "git" in message.content and not message.author.bot:
+        if "$joke" in message.content:
+            await message.channel.send(random_joke())
+        elif "git" in message.content and not message.author.bot:
             await message.channel.send("Excuse me, would you like to hear about our lord and savior Git, the free and open source version control system? https://git-scm.com/")
         elif "mercurial" in message.content:
             await message.channel.send("Ewwwwwww blasphemy. Imagine being so dense")
